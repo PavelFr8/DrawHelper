d891a2a4b197035682bc5f902de68abe85e4e69c
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-06-12 20:09:10 +0300

    fix

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Запуск скрипта в фоновом режиме
-subprocess.Popen(['python', 'create_shortcut.py'], creationflags=subprocess.CREATE_NO_WINDOW)
+subprocess.Popen(['python', 'funcs/create_shortcut.py'], creationflags=subprocess.CREATE_NO_WINDOW)
 subprocess.Popen(['python', 'main.py'], creationflags=subprocess.CREATE_NO_WINDOW)
 
 # python -m PyInstaller --noconsole --onefile run.py
