53d22d61aea0da073d4c0b07c5f85a97821ced04
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-06-12 16:32:19 +0300

    1-st prototype

---
+++ main.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtGui import QPainter, QPen, QMouseEvent, QImage, QKeyEvent, QColor
+from PyQt6.QtCore import Qt, QPoint, QSize
+import ctypes
+import time
+
+user32 = ctypes.windll.user32
+w, h = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+class DrawingWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
+        self.drawing = False
+        self.last_point = QPoint()
+        self.image = QImage(QSize(w, h - 50), QImage.Format.Format_ARGB32)
+        self.image.fill(QColor(0, 0, 0, 100))
+        self.setFixedSize(w, h)  # Установите желаемый размер окна
+
+
+    def mousePressEvent(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.drawing = True
+            self.last_point = event.position().toPoint()
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        if (event.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(Qt.GlobalColor.black, 10, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
+            painter.drawLine(self.last_point, event.position().toPoint())
+            self.last_point = event.position().toPoint()
+            self.update()
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton and self.drawing:
+            self.drawing = False
+
+    def paintEvent(self, event):
+        canvas_painter = QPainter(self)
+        canvas_painter.drawImage(0, 0, self.image)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("DrawHelper")
+
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        central_widget = QWidget(self)
+        central_layout = QVBoxLayout(central_widget)
+
+        drawing_widget = DrawingWidget()
+
+        central_layout.addWidget(drawing_widget)
+
+        self.setCentralWidget(central_widget)
+
+        self.setGeometry(1, 1, w, h - 50)
+
+
+
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_Escape:
+            self.close()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
