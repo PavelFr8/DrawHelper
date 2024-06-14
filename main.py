6ab66edce2a9ee045b275d3ee237151a919ec86f
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-06-15 01:37:42 +0300

    trying add menu

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTabWidget, QHBoxLayout, QPushButton, \
+    QLabel
 from PyQt6.QtGui import QPainter, QPen, QMouseEvent, QImage, QKeyEvent, QColor
 from PyQt6.QtCore import Qt, QPoint, QSize
 import ctypes
 
+# get the size of users screen.
 user32 = ctypes.windll.user32
 w, h = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
+
+# widget which is works as area where user draw
 class DrawingWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
         self.drawing = False
         self.last_point = QPoint()
         self.image = QImage(QSize(w, h - 50), QImage.Format.Format_ARGB32)
         self.image.fill(QColor(0, 0, 0, 100))
-        self.setFixedSize(w, h)  # Установите желаемый размер окна
+        self.setFixedSize(w, h)
 
-
+    # funcs for drawing
     def mousePressEvent(self, event: QMouseEvent):
         if event.button() == Qt.MouseButton.LeftButton:
             self.drawing = True
@@ -40,30 +44,61 @@ class DrawingWidget(QWidget):
         canvas_painter.drawImage(0, 0, self.image)
 
 
+# the menu
+class MenuWidget(QTabWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(250, 100)
+
+        # Tab 1 - Clear
+        tab1 = QWidget()
+        tab1_layout = QVBoxLayout()
+        tab1_layout.addWidget(QLabel("1"))
+        tab1.setLayout(tab1_layout)
+
+        # Tab 2 - Color
+        tab2 = QWidget()
+        tab2_layout = QVBoxLayout()
+        tab2_layout.addWidget(QLabel("2"))
+        tab2.setLayout(tab2_layout)
+
+        # Tab 3 - Size
+        tab3 = QWidget()
+        tab3_layout = QVBoxLayout()
+        tab3_layout.addWidget(QLabel("3"))
+        tab3.setLayout(tab3_layout)
+
+        # Add tabs to QTabWidget
+        self.addTab(tab1, "  Очистить  ")
+        self.addTab(tab2, "    Цвет    ")
+        self.addTab(tab3, "   Размер   ")
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("DrawHelper")
-
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setGeometry(1, 1, w, h - 50)
 
         central_widget = QWidget(self)
-        central_layout = QVBoxLayout(central_widget)
-
+        central_layout = QHBoxLayout(central_widget)
         drawing_widget = DrawingWidget()
-
         central_layout.addWidget(drawing_widget)
+        central_widget.setLayout(central_layout)
 
         self.setCentralWidget(central_widget)
 
-        self.setGeometry(1, 1, w, h - 50)
+        self.menu_widget = MenuWidget()
+        self.menu_widget.setParent(self)
+        self.menu_widget.setGeometry(1, 1, 200, h - 50)
 
-
-
+    # Function to close the app
     def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key.Key_Escape:
             self.close()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
