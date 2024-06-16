0465dab713ae32a5f343f36f902f245cea55fe64
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-06-16 18:53:20 +0300

    add new drawing modes

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ class DrawingWidget(QWidget):
         self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
         self.drawing = False
         self.last_point = QPoint()
+        self.start_point = QPoint()
+        self.temp_image = None
         self.setFixedSize(w, h)
+        self.mode = 'pen'
 
         # load save data
         data = get_save()
@@ -37,24 +40,64 @@ class DrawingWidget(QWidget):
     # next funcs for drawing
     def mousePressEvent(self, event: QMouseEvent):
         if event.button() == Qt.MouseButton.LeftButton:
-            self.drawing = True
-            self.last_point = event.position().toPoint()
+            if self.mode == 'pen':
+                self.drawing = True
+                self.last_point = event.position().toPoint()
+            elif self.mode == 'circle' or self.mode == 'line':
+                if not self.drawing:
+                    self.drawing = True
+                    self.start_point = event.position().toPoint()
+                    self.temp_image = self.image.copy()
+                else:
+                    self.drawing = False
+                    self.image = self.temp_image
+                    self.update()
+
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if (event.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
-            painter = QPainter(self.image)
-            painter.setPen(QPen(self.color, self.paint_size, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
-            painter.drawLine(self.last_point, event.position().toPoint())
-            self.last_point = event.position().toPoint()
-            self.update()
+            if self.mode == 'pen':
+                painter = QPainter(self.image)
+                painter.setPen(QPen(self.color, self.paint_size, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap,
+                                    Qt.PenJoinStyle.RoundJoin))
+                current_point = event.position().toPoint()
+                painter.drawLine(self.last_point, current_point)
+                self.last_point = current_point
+                self.update()
+
+            elif self.mode == 'circle':
+                self.temp_image = self.image.copy()
+                painter = QPainter(self.temp_image)
+                painter.setPen(QPen(self.color, self.paint_size, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap,
+                                    Qt.PenJoinStyle.RoundJoin))
+                current_point = event.position().toPoint()
+                radius = (self.start_point - current_point).manhattanLength()
+                painter.drawEllipse(self.start_point, radius, radius)
+                self.update()
+
+            elif self.mode == 'line':
+                self.temp_image = self.image.copy()
+                painter = QPainter(self.temp_image)
+                painter.setPen(QPen(self.color, self.paint_size, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap,
+                                    Qt.PenJoinStyle.RoundJoin))
+                painter.drawLine(self.start_point, event.position().toPoint())
+                self.update()
 
     def mouseReleaseEvent(self, event: QMouseEvent):
         if event.button() == Qt.MouseButton.LeftButton and self.drawing:
             self.drawing = False
+            if self.mode == 'circle':
+                self.image = self.temp_image
+            if self.mode == 'line':
+                self.image = self.temp_image
+                self.update()
 
     def paintEvent(self, event):
         canvas_painter = QPainter(self)
-        canvas_painter.drawImage(0, 0, self.image)
+        if self.temp_image and (self.mode == 'circle' or self.mode == 'line') and self.drawing:
+            canvas_painter.drawImage(0, 0, self.temp_image)
+        else:
+            canvas_painter.drawImage(0, 0, self.image)
 
 
 # the menu class
@@ -62,7 +105,7 @@ class MenuWidget(QTabWidget):
     def __init__(self, draw_area):
         super().__init__()
         self.draw_area = draw_area
-        self.setFixedSize(288, 115)
+        self.setFixedSize(260, 115)
         self.setStyleSheet("""
             font-weight: bold;
             font-size: 13px;
@@ -101,6 +144,22 @@ class MenuWidget(QTabWidget):
         slider.valueChanged.connect(self.change_size)
         tab2_layout.addWidget(slider)
 
+        buttons = QWidget()
+        buttons_layout = QVBoxLayout()
+
+        circle_button = QPushButton('Circle')
+        circle_button.clicked.connect(lambda: self.draw_mode_change('circle'))
+        line_button = QPushButton('Line')
+        line_button.clicked.connect(lambda: self.draw_mode_change('line'))
+        pen_button = QPushButton('Pen')
+        pen_button.clicked.connect(lambda: self.draw_mode_change('pen'))
+
+        buttons_layout.addWidget(circle_button)
+        buttons_layout.addWidget(line_button)
+        buttons_layout.addWidget(pen_button)
+        buttons.setLayout(buttons_layout)
+        tab2_layout.addWidget(buttons)
+
         tab2.setLayout(tab2_layout)
 
         # Tab 3 - Color
@@ -111,7 +170,7 @@ class MenuWidget(QTabWidget):
         self.addTab(tab0, "Settings")
         self.addTab(tab1, "Clear")
         self.addTab(tab3, "Color")
-        self.addTab(tab2, " Size ")
+        self.addTab(tab2, " Modes ")
 
     # func which is clear drawings
     def clear(self, index):
@@ -130,6 +189,7 @@ class MenuWidget(QTabWidget):
     def change_size(self, value):
         self.draw_area.paint_size = value / 4
 
+    # func which is change background opacity
     def change_opacity(self, value):
         self.draw_area.opacity = int(value * 2.5)
         if self.draw_area.opacity == 0:
@@ -137,9 +197,14 @@ class MenuWidget(QTabWidget):
         self.draw_area.image.fill(QColor(0, 0, 0, self.draw_area.opacity))
         self.draw_area.update()
 
+    # the func for exit from the app
     def exit(self):
         create_save(self.draw_area)
         sys.exit(app.exec())
+
+    # change drawing modes
+    def draw_mode_change(self, mode):
+        self.draw_area.mode = mode
 
 
 class MainWindow(QMainWindow):
